770c41f3e09047122d023ef61b5915a17593e304
zcvaters <zachvaters@gmail.com>
2021-03-08 16:38:07 -0330

    Add more request tests PogChamp

--- test/testRequests.js
+++ test/testRequests.js
@@ -6,7 +6,7 @@
 
 const assert = require("assert");
 const axios = require("axios");
-const { provinces, sources } = require("../utils/consts");
+const { provinces, sources, toxins } = require("../utils/consts");
 
 const url = "http://localhost:3000";
 
@@ -32,6 +32,19 @@ describe("Testing pollution API requests with valid schemas", async function () 
         );
       });
     });
+    it("Success 2 - Sending request to /stats/pie with filter on toxins grouped by Source", async function () {
+      return postRequest("/stats/pie", {
+        filters: { toxins: ["CO"] },
+        groupedBy: ["Source"],
+      }).then((res) => {
+        assert.strictEqual(res.data.err, undefined);
+
+        assert.strictEqual(
+          sources.includes(res.data.result[0]._id.Source),
+          true
+        );
+      });
+    });
   });
   describe("Testing /stats/heatmap requests", async function () {
     it("Success 1 - Sending request to /stats/heatmap with year range", async function () {
@@ -48,6 +61,15 @@ describe("Testing pollution API requests with valid schemas", async function () 
         assert.strictEqual(Object.keys(res.data.result[0]._id).length, 1);
       });
     });
+    it("Success 2 - Sending request to /stats/heatmap year start bound and CO toxin only", async function () {
+      return postRequest("/stats/heatmap", {
+        filters: {yearStart: 2000, toxins: ["CO"]},
+      }).then((res) => {
+        assert.strictEqual(res.data.err, undefined);
+        res.data.result.forEach((stat) => assert.notStrictEqual(stat.CO, undefined));
+        assert.strictEqual(res.data.result[0].TPM, undefined);
+      });
+    });
   });
 
   describe("Testing /stats/bar requests", async function () {
@@ -56,6 +78,8 @@ describe("Testing pollution API requests with valid schemas", async function () 
         filters: { yearStart: 2001, yearEnd: 2010 },
         groupedBy: ["Region"],
       }).then((res) => {
+        assert.strictEqual(res.data.err, undefined);
+
         assert.notStrictEqual(res.data.result, undefined);
 
         // check for presence of each region in result
@@ -64,6 +88,19 @@ describe("Testing pollution API requests with valid schemas", async function () 
             res.data.result.find((stat) => stat._id?.Region === province),
             undefined
           )
+        );
+      });
+    });
+    it("Success 2 - Sending request to /status/bar with yearStart and grouped by Source", async function () {
+      return postRequest("/stats/bar", {
+        filters: { yearStart: 2001 },
+        groupedBy: ["Source"],
+      }).then((res) => {
+        assert.strictEqual(res.data.err, undefined);
+
+        assert.strictEqual(
+          sources.includes(res.data.result[0]._id.Source),
+          true
         );
       });
     });
@@ -78,6 +115,17 @@ describe("Testing pollution API requests with valid schemas", async function () 
         assert.notStrictEqual(res.data.result.NL, undefined);
 
         assert.strictEqual(Object.keys(res.data.result.NL[0]._id).length, 1);
+      });
+    });
+    it("Success 2 - Sending a request to /stats/timeseries with region lock source filter", async function () {
+      return postRequest("/stats/timeseries", {
+        filters: { regions: ["NL"], toxins: ["CO"] },
+      }).then((res) => {
+        assert.strictEqual(res.data.err, undefined);
+        res.data.result.NL.forEach((stat) =>
+          assert.strictEqual(stat.CO > 0, true)
+        );
+        assert.notStrictEqual(res.data.result.NL, undefined);
       });
     });
   });
